7040652af4db400aae5b3af83788a657fb3b0f4a
Gulshan Jubaed Prince <jubaedprince@hotmail.com>
2017-06-14 00:07:22 +0600

    google webmaster verfication code added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="-VTlQvpwyVlTSCuvCiSLl696V7E2Dk-cEHgBe4-svAQ" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">

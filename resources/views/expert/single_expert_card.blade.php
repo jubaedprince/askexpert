7b50596ddf6a29ea0d4e78bc4515a1731e654b33
Gulshan Jubaed Prince <jubaedprince@hotmail.com>
2017-06-15 00:50:33 +0600

    clickable area for expert card expanded

--- resources/views/expert/single_expert_card.blade.php
+++ resources/views/expert/single_expert_card.blade.php
@@ -1,15 +1,15 @@
 <div class="expert">
     <!-- <img class="img-rounded" src="http://placehold.it/180X180"> -->
-    <img class="img-rounded" src="{{$expert->profile_picture_url}}">
+    <a href="/{{$expert->slug}}"><img class="img-rounded" src="{{$expert->profile_picture_url}}"></a>
     <a href="/{{$expert->slug}}">
         <div class="expert-name">{{$expert->user->name}}</div>
     </a>
-    <div class="expert-title"><p>{{$expert->current_occupation}}</p></div>
-    <div class="expert-caption">
+    <a href="/{{$expert->slug}}"><div class="expert-title"><p>{{$expert->current_occupation}}</p></div></a>
+    <a href="/{{$expert->slug}}"><div class="expert-caption">
         <p>
             {{str_limit($expert->bio, 350)}}
         </p>
-    </div>
+    </div> </a>
     {{--<button class="btn btn-expert" data-toggle="modal" data-target="#myModal" onClick="setGlobalExpert({{$expert->id}}, {{$expert->cost_per_minute}})">Set Meeting Tk.{{$expert->cost_per_minute}}/minute</button>--}}
     <button class="btn btn-expert" ng-click="setMeetingClicked({{$expert->id}})">Set Meeting Tk.{{$expert->cost_per_minute}}/minute</button>
 </div>

c901040ec19e9dd3bff120cb0c817c724612ddf9
Gulshan Jubaed Prince <jubaedprince@hotmail.com>
2017-06-14 23:56:32 +0600

    profile page modified

--- resources/views/expert/profile.blade.php
+++ resources/views/expert/profile.blade.php
@@ -2,72 +2,52 @@
 
 @section('styles')
     <style>
-        iframe, object, embed {
-            max-width: 100%;
-        }
 
-        .profile-pic-panel{
-            margin-right: 10px;
-        }
-
-        .service-title-header{
-            margin-top: 2px;
-        }
-
-        .set-meeting-button-instruction{
-            color: #858585;
-            padding: 3px;
-            font-size:13px;
-        }
     </style>
 @endsection
 
 @section('content')
-    <!-- <img class="img-rounded" src="http://placehold.it/180X180"> -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-md-offset-2">
+                <img class="img-rounded pull-right img-responsive" src="{{$expert->profile_picture_url}}" style="width: 100%">
+
+            </div>
+            <div class="col-md-6">
+                <div class="expert-name">{{$expert->user->name}}</div>
+                <div class="expert-title"><p>{{$expert->current_occupation}}</p></div>
+
+
+                <p>{{$expert->bio}}</p>
+                <button class="btn btn-expert" style="margin-bottom: 20px"
+                        ng-click="setMeetingClicked({{$expert->id}})">Set Meeting
+                    Tk.{{$expert->cost_per_minute}}/minute
+                </button>
+            </div>
+
+        </div>
+
+
+    </div>
+
+    <div class="row">
+        @if($expert->youtube_video_id)
+            <div class="text-center">
+                <div class="video-container">
+                    <iframe width="640" height="360" src="https://www.youtube.com/embed/{{$expert->youtube_video_id}}" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        @endif
+    </div>
 
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
 
             <div class="row">
-                <div class="col-md-3">
-                    <div class="row">
-                        {{--profile pic and expert info--}}
-                        <div class="panel panel-default profile-pic-panel">
-                            <div class="panel-body text-center">
-                                <div class="row">
-                                    <img class="img-rounded" src="{{$expert->profile_picture_url}}">
-                                    <div class="expert-name">{{$expert->user->name}}</div>
-                                    <div class="expert-title"><p>{{$expert->current_occupation}}</p></div>
-                                    <button class="btn btn-expert"
-                                            ng-click="setMeetingClicked({{$expert->id}})">Set Meeting
-                                        Tk.{{$expert->cost_per_minute}}/minute
-                                    </button>
-                                    <p class="set-meeting-button-instruction">If you set a meeting, expert will talk to you now</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-9">
-                    {{--expert bio & video--}}
-                    <div class="row">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <p>{{$expert->bio}}</p>
-                                        @if($expert->youtube_video_id)
-                                            <div class="row text-center">
-                                                <div class="video-container">
-                                                    <iframe width="640" height="360" src="https://www.youtube.com/embed/{{$expert->youtube_video_id}}" frameborder="0" allowfullscreen></iframe>
-                                                </div>
-                                            </div>
-                                        @endif
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+
+
                     {{--area of expertise--}}
                     <div class="row">
                         <div class="col-md-12">
@@ -98,6 +78,7 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>

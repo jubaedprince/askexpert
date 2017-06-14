4737f2a3b99a415ee8628a5dd94e5cb82f669a34
Gulshan Jubaed Prince <jubaedprince@hotmail.com>
2017-06-15 00:31:37 +0600

    font size tweak

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
     <link href="/css/carousel.css" rel="stylesheet">
 
     <link rel="stylesheet" href="/external-libraries/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     @yield('styles')
 
